08971610b2160944c6cbe7108962566fbad5a985
David Rath <davidrath828@gmail.com>
2023-06-19 13:50:16 +0200

    Added Speed Test, Scaling and Information Tabs

--- Interface.py
+++ Interface.py
@@ -2,6 +2,7 @@ import torch
 import gradio as gr
 import json
 import time
+from tqdm import tqdm
 
 import architecture.ShuffleNetV1.network as ShuffleNetV1
 import architecture.ShuffleNetV2.network as ShuffleNetV2
@@ -207,12 +208,57 @@ def predict_all(img):
 
     return list(predictions.values())
 
+# test the speed of a model
+def speed_test_single(img):
+
+    # crop the image
+    img = center_crop(img)
+    input_tensor = torch.from_numpy(img).unsqueeze(0).permute(0, 3, 1, 2).float()
+
+    time_sum = 0
+    inferences = 100
+
+    # a for loop that uses tqdm:
+    for i in tqdm(range(inferences)):
+
+        # Get the prediction
+        _, time = timed_prediction(input_tensor)
+        time_sum += time
+
+    time_avg = time_sum / inferences
+
+    return time_avg, time_sum
+
+# run speed_test_single on all models
+# this function uses the function speed_test_single(img)
+def speed_test_full(img):
+    
+    times = {}
+
+    for configuration in configurations:
+        # load the model
+        load_model(configuration)
+
+        # Get the prediction
+        time_avg, time_sum = speed_test_single(img)
+
+        # turn it into a nice string
+        text_time_avg = str(round(time_avg, 5)) + "s"
+        text_time_sum = str(round(time_sum, 5)) + "s"
+
+        times[configuration] = text_time_sum
+
+    return list(times.values())
+
+
 
 custom_css = """
 #row1 {height: 60vh !important;overflow-y: auto;}
 #row2 {height: 80vh !important;overflow-y: auto;}
-#label_conf {height: 10vh !important; padding: 0px !important;}
-#label_conf .output-class {font-size: var(--text-lg) !important;}
+#label_conf_inference {height: 10vh !important; padding: 0px !important;}
+#label_conf_inference .output-class {font-size: var(--text-lg) !important;}
+#label_conf_speed {height: 10vh !important; padding: 0px !important;}
+#label_conf_speed .output-class {font-size: 25pt !important;}
 """
 
 with gr.Blocks(css=custom_css) as demo:
@@ -232,7 +278,7 @@ with gr.Blocks(css=custom_css) as demo:
 
         with gr.Row(elem_id="row1"):
             with gr.Column(scale = 5):
-                input_image_single = gr.Image(shape=(224, 224))
+                input_image_single = gr.Image(shape=(224, 224), tool=None)
 
             with gr.Column(scale = 2):
                 label_prediction = gr.Label("-", label="Prediction")
@@ -248,19 +294,96 @@ with gr.Blocks(css=custom_css) as demo:
 
         with gr.Row(elem_id="row2"):
             with gr.Column(scale = 4):
-                input_image_full = gr.Image(shape=(224, 224))
+                input_image_full = gr.Image(shape=(224, 224), tool=None)
 
             with gr.Column(scale = 1):
                 for configuration in configurations[:6]:
-                    conf_labels[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf")
+                    conf_labels[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf_inference")
 
             with gr.Column(scale = 1):
                 for configuration in configurations[6:]:
-                    conf_labels[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf")
+                    conf_labels[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf_inference")
 
         btn_predict_full = gr.Button("Predict")
         btn_predict_full.click(predict_all, inputs=input_image_full, outputs=list(conf_labels.values()))
         input_image_full.change(predict_all, inputs=input_image_full, outputs=list(conf_labels.values()))
+
+    # here we compare the speed of the models
+    with gr.Tab("Speed Test"):
+
+        conf_labels_speed = {}
+
+        with gr.Row(elem_id="row2"):
+            with gr.Column(scale = 4):
+                input_image_speed = gr.Image(shape=(224, 224), tool=None)
+
+            with gr.Column(scale = 1):
+                for configuration in configurations[:6]:
+                    conf_labels_speed[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf_speed")
+
+            with gr.Column(scale = 1):
+                for configuration in configurations[6:]:
+                    conf_labels_speed[configuration] = gr.Label("-", label=configuration, elem_id = "label_conf_speed")
+
+        btn_predict_speed = gr.Button("Predict")
+        btn_predict_speed.click(speed_test_full, inputs=input_image_speed, outputs=list(conf_labels_speed.values()))
+
+        gr.Markdown("### Speed Test")
+        gr.Markdown("This test does 100 inferences of a 224x224 image and shows the total time for all inferences. This may take a while!")
+
+    with gr.Tab("Scaling"):
+
+        with gr.Row(elem_id="row2"):
+            with gr.Column(scale = 1):
+                input_image_scale = gr.Image(shape=(224, 224), tool=None)
+
+            with gr.Column(scale = 1):
+                output_image_scale = gr.Image(tool=None)
+
+        btn_predict_scale = gr.Button("Scale to 224x224")
+        btn_predict_scale.click(center_crop, inputs=input_image_scale, outputs=output_image_scale)
+        input_image_scale.change(center_crop, inputs=input_image_scale, outputs=output_image_scale)
+
+    with gr.Tab("Information"):
+        gr.Markdown("""
+        # Model Information
+        Both the model architecture and the weights are taken from the [ShuffleNet-Series GitHub Repo](https://github.com/megvii-model/ShuffleNet-Series).
+
+        ## ShuffleNet V1
+        ShuffleNet V1 is a convolutional neural network designed for efficient computation, 
+        particularly focusing on mobile devices with very limited computing power. The network 
+        gets its name from a novel operation that it introduces: channel shuffle. This operation 
+        allows for cross-channel information flow which in turn enables the building of more 
+        powerful structures with a fraction of computation cost. The model achieves this by using 
+        pointwise group convolutions and channel shuffle, drastically reducing computation cost 
+        while maintaining accuracy. Various versions of the model are created by varying the 
+        number of groups.
+
+        [arxiv.org](https://arxiv.org/abs/1707.01083)
+
+        ## ShuffleNet V2
+        ShuffleNet V2 improves on V1 by considering direct and indirect metrics to measure the 
+        network's speed and designing the architecture accordingly. The network considers aspects 
+        such as memory access cost, parallelism, and channel selection for the network's speed. 
+        The resulting model is even more efficient than V1 for a range of model sizes. It 
+        eliminates the pointwise group convolutions and maintains the channel shuffle operation. 
+        The improved performance makes it particularly suitable for real-time applications on 
+        mobile devices.
+
+        [arxiv.org](https://arxiv.org/abs/1807.11164)
+
+        ## Dataset
+        The model is trained on the ImageNet dataset, which consists of 1.2 million images with
+        1000 classes. The images are of varying sizes and aspect ratios. Both models are trained on
+        images that are center cropped to 224x224. Images are normalized using the mean and standard 
+        deviation of the ImageNet dataset. The label names are taken from the ```imagenet-simple-labels```
+        repository on GitHub.
+
+        [image-net.org](http://www.image-net.org/) [imagenet-simple-labels](https://github.com/anishathalye/imagenet-simple-labels/tree/master)
+        
+        Built by David Rath (david.rath@studium.uni-hamburg.de)
+        
+        """)
 
 # load the default model
 load_model('s2_10')
